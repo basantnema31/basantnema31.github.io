38c66b1a9c26a3df4f45c995d1214fb866ff138f
basantnema31 <pianistbasant31@gmail.com>
2025-09-20 01:02:12 +0530

    Scrolling Changes

--- Assets/JS/Scroll Btn.js
+++ Assets/JS/Scroll Btn.js
@@ -30,7 +30,3 @@ window.addEventListener("scroll", () => {
   checkScroll();
 });
 
-// Smooth scroll to top on click
-scrollBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
