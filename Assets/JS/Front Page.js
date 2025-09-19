63997aa5b7637b90ca96df6b46a8a45add521c2c
basantnema31 <pianistbasant31@gmail.com>
2025-09-19 19:09:28 +0530

    Added Effects & Major Changes

--- Assets/JS/Front Page.js
+++ Assets/JS/Front Page.js
@@ -1,7 +1,31 @@
+// Skills Effect
 var typed = new Typed(".text", {
     strings: ["Coder", "YouTuber", "Video/Audio Editor"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
+});
+
+
+// #home to #portfolio in url
+const portfolioLogoLink = document.querySelector('.logo');
+const homeSection = document.querySelector('#home');
+
+portfolioLogoLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    homeSection.scrollIntoView({ behavior: 'smooth' });
+    history.pushState(null, null, '#portfolio');
+});
+
+
+// Hover Sound Effect
+const imagesWithSound = document.querySelectorAll('.home img, .about img');
+const hoverSound = document.getElementById('hover-sound');
+
+imagesWithSound.forEach(image => {
+    image.addEventListener('mouseenter', () => {
+        hoverSound.currentTime = 0;
+        hoverSound.play();
+    });
 });
